ea3dfc8d55d2ac244a34c8179af5725bd76e2160
PaigeN2026 <Paige.nunez2026@gbwl.org>
2024-12-10 13:54:51 +0000

    added constructors to the Student class

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Constructing Student objects...");
+
+      // Call DEFAULT CONSTRUCTOR
+      // "Student senior" is the type declaration 
+      // "= new Student()" is calling the constructor 
+      Student kinder = new Student();
+
+      // Call CONSTRUCTOR with PARAMETERS
+      Student junior = new Student("Paige", "Nunez", 2026);
       
    }
 }
--- Student.java
+++ Student.java
@@ -7,14 +7,29 @@ public class Student {
     // 1. INSTANCE VARIABLES (attributes/data/fields)
     // think of what this object "needs to know"
     // must be PRIVATE ("encapsulation")
-    private String name;
+    private String firstName;
+    private String lastName;
     private int gradYear;
     private String email; 
-    private boolean sports;
-    private double gpa;
-
 
     // 2. CONSTRUCTORS (manuals for setting up an object)
+    // DEFAULT constructor (user did not provide data)
+    // Job: initialize instance variables to default values 
+    public Student() {
+        firstName = "First";
+        lastName= "Last"
+        gradYear = 2037;
+        email = firstName + "." + lastName + gradYear + "@gbwl.org";
 
+    }
+
+    // Constructor with PARAMETERS (user provided key data)
+    // Job: initialize instance variables to PROVIDED values
+    public Student(String initFName, String initLName, int initGradYear) {
+        firstName = initFName;
+        lastName = initLName;
+        gradYear = initGradYear;
+        email = firstName + "." + lastName + gradYear + "@gbwl.org";
+    }
     // 3. METHODS (behaviors associated with the object type)
 } 
