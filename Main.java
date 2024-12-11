0438e772766235c0d75af6e906c7919b785c3c53
PaigeN2026 <Paige.nunez2026@gbwl.org>
2024-12-11 14:38:17 +0000

    defined toString method

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+   // This is the "Tester Class" with a main method
    public static void main(String []args) {
       System.out.println("Constructing Student objects...");
 
@@ -8,8 +9,21 @@ public class Main {
       // "= new Student()" is calling the constructor 
       Student kinder = new Student();
 
-      // Call CONSTRUCTOR with PARAMETERS
+      // Call CONSTRUCTOR with PARAMETERS (String, String, int)
       Student junior = new Student("Paige", "Nunez", 2026);
+      // Does argument order matter? YES!
+      // Student senior = new Student(2025, "Zoie", "Vitale");
+
+      // Look at our constructed Student objects
+      System.out.println(kinder); // will print the LOCATION of the object
+      // ...unless our class has a toString method defined!
+      System.out.println(junior);
+
+      // Construct two more Student objects & print them out
+      Student senior = new Student("Zoie", "Vitale", 2025);
+      System.out.println(senior);
       
+      Student sophomore = new Student("Soph", "Omore", 2027);
+      System.out.println(sophomore);
    }
 }
--- Student.java
+++ Student.java
@@ -14,10 +14,11 @@ public class Student {
 
     // 2. CONSTRUCTORS (manuals for setting up an object)
     // DEFAULT constructor (user did not provide data)
+    // "no-argument" constructor 
     // Job: initialize instance variables to default values 
     public Student() {
         firstName = "First";
-        lastName= "Last"
+        lastName= "Last";
         gradYear = 2037;
         email = firstName + "." + lastName + gradYear + "@gbwl.org";
 
@@ -25,11 +26,18 @@ public class Student {
 
     // Constructor with PARAMETERS (user provided key data)
     // Job: initialize instance variables to PROVIDED values
-    public Student(String initFName, String initLName, int initGradYear) {
-        firstName = initFName;
-        lastName = initLName;
+    // ORDER OF PARAMETERS IS IMPORTANT! (the data types)
+    // "initFirstName," etc. are like PLACEHOLDERS for real values 
+    public Student(String initFirstName, String initLastName, int initGradYear) {
+        firstName = initFirstName;
+        lastName = initLastName;
         gradYear = initGradYear;
         email = firstName + "." + lastName + gradYear + "@gbwl.org";
     }
     // 3. METHODS (behaviors associated with the object type)
+    // toString method provides a "String Representation"
+    // of an object instance with all its data 
+    public String  toString() {
+        return ("Student [" + firstName + ", " + lastName + ", " + gradYear + ", " + email + "]");
+    }
 } 
